35e57aa788afe09a6b65485bb7e0b92ebd3e7fd7
luc662 <31988325+luc662@users.noreply.github.com>
2023-04-16 18:48:44 -0300

    Add files via upload
    nuevo cliente servidor basico con UDP y creando un socket nuevo para escuchar

--- cliente-servidor-basico/client.py
+++ cliente-servidor-basico/client.py
@@ -6,15 +6,14 @@ port = 2004
 BUFFER_SIZE = 2000
 MESSAGE = input("tcpClientA: Enter message/ Enter exit:")
 
-tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tcpClientA.connect((host, port))
 if MESSAGE == 'exit':
-    tcpClientA.send(MESSAGE.encode())
+    tcpClientA.sendto(MESSAGE.encode(), (host, port))
 
 while MESSAGE != 'exit':
-    tcpClientA.send(MESSAGE.encode())
+    tcpClientA.sendto(MESSAGE.encode(), (host, port))
     data = tcpClientA.recv(BUFFER_SIZE)
     print (" Client2 received data:", data.decode())
     MESSAGE = input("tcpClientA: Enter message to continue/ Enter exit:")
 
-tcpClientA.close()
